9105d3155ec55057075f431b96ed6b6559d58ff2
CaiXiangWei <1281735549@qq.com>
2019-02-18 17:53:48 +0800

    画板功能完成

--- app/src/main/kotlin/com/wei/painting/view/PaintView.kt
+++ app/src/main/kotlin/com/wei/painting/view/PaintView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.MotionEvent
+import android.view.View
 import android.widget.ImageView
 import com.wei.painting.R
 
@@ -14,51 +15,19 @@ import com.wei.painting.R
  */
 class PaintView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet?= null, defStyleAttr: Int = 0
-) : ImageView(context, attrs, defStyleAttr) {
+) : View(context, attrs, defStyleAttr) {
 
     private var paint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.DITHER_FLAG)
     private var path = Path()
     private lateinit var bufferBitmap: Bitmap
     private lateinit var bufferCanvas: Canvas
+
     private var lastX = 0.0f
     private var lastY = 0.0f
 
     init {
-        setBackgroundResource(R.drawable.ic_paint_bg)
         initPaint()
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        bufferBitmap = Bitmap.createBitmap(measuredWidth, measuredHeight, Bitmap.Config.ARGB_8888)
-        bufferCanvas = Canvas(bufferBitmap)
-    }
-
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        canvas?.drawBitmap(bufferBitmap, 0f, 0f, null)
-    }
-
-    override fun onTouchEvent(event: MotionEvent?): Boolean {
-        event?.let {
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    lastX = event.x
-                    lastY = event.y
-
-                    path.moveTo(lastX, lastY)
-                }
-
-                MotionEvent.ACTION_MOVE -> {
-                    path.quadTo(lastX, lastY, event.x, event.y)
-                }
-
-                MotionEvent.ACTION_UP -> {
-
-                }
-            }
-        }
-        return true
+        initBg()
     }
 
     private fun initPaint() {
@@ -67,6 +36,52 @@ class PaintView @JvmOverloads constructor(
         paint.strokeJoin = Paint.Join.ROUND // 使画笔更加圆润
         paint.strokeCap = Paint.Cap.ROUND // 同上
         paint.strokeWidth = 10f
+    }
+
+    private fun initBg() {
+//        setBackgroundColor(Color.WHITE)
+        setBackgroundResource(R.drawable.ic_paint_bg)
+    }
+
+//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+//        bufferBitmap = Bitmap.createBitmap(measuredWidth, measuredHeight, Bitmap.Config.ARGB_8888)
+//        bufferCanvas = Canvas(bufferBitmap)
+//    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+//        canvas?.drawBitmap(bufferBitmap, 0f, 0f, null)
+        canvas?.drawPath(path, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> { // 手指按下时触发，每次触摸事件只会触发一次 ACTION_DOWN
+                // 将起始点移动到当前坐标
+                path.moveTo(event.x, event.y)
+
+                // 记录上次触摸的坐标
+                lastX = event.x
+                lastY = event.y
+            }
+
+            MotionEvent.ACTION_MOVE -> { // 手指移动的时候触发，每次触摸事件可能多次触发 ACTION_MOVE
+                //绘制圆滑曲线，即贝塞尔曲线
+                path.quadTo(lastX, lastY, event.x, event.y)
+
+                lastX = event.x
+                lastY = event.y
+            }
+
+            MotionEvent.ACTION_UP -> { // 手指离开屏幕时触发，每次触摸事件只会触发一次 ACTION_UP
+
+            }
+        }
+
+        // 重新绘制，会调用 onDraw 方法
+        invalidate()
+        return true
     }
 
 }
